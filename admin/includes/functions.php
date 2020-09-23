b8fa2587bf7e2cb6a9db7d5f957ecbb42eb9ec39
marierabill <71690762+marierabill@users.noreply.github.com>
2020-09-23 09:51:29 +0300

    Update functions.php
    function delete_category

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -49,4 +49,27 @@ function show_category()
 	}
 }
 
+function delete_category()
+{
+	global $connection;
+	if(isset($_GET['delete_cat']))
+	{
+		$cat_id = $_GET['delete_cat'];
+		$query = "DELETE FROM categories WHERE cat_id = $cat_id";
+		$result = mysqli_query($connection, $query);
+		
+		if(!$result)
+				{
+					die("Could not delete data " . mysqli_error($connection));
+				}else
+					{
+						header("Location: ../categories.php?category_deleted");
+					}
+		
+	}
+	
+}
+delete_category();
+
+
 ?>
