091de63c61b82393288484865d00d4b23d30c658
marierabill <71690762+marierabill@users.noreply.github.com>
2020-09-23 11:47:42 +0300

    Update add_post.php

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,47 +1,54 @@
-<div id="wrapper">
-
-	<!-- Navigation -->
-	<?php include 'includes/navigation.php'; ?>
-
-
-	<div id="page-wrapper">
-
-		<div class="container-fluid">
-
-			<!-- Page Heading -->
-			<div class="row">
-
+<h2>Add Posts</h2>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6">
+			<form action="" method="post">
+				<div class="form-group">
+					<label for="">Post Title</label>
+					<input type="text" name="title" placeholder="Post Title" class="form-control"> 
+				</div>
 				
-				<h2>
-					Add Posts
-				</h2>
+				<div class="form-group">
+					<label for="">Post Author</label>
+					<input type="text" name="author" placeholder="Post Author" class="form-control"> 
+				</div>
 				
-			</div>
+				<div class="form-group">
+					<label for="">Post Category</label>
+					<select class="form-control" name="category">
+						<option value=""></option>
+					</select>
+				</div>
+				
+				<div class="form-group">
+					<label for="">Post Category ID</label>
+					<select class="form-control" name="category_id">
+						<option value=""></option>
+					</select>
+				</div>
+				
+				<div class="form-group">
+					<label for="">Post Tags</label>
+					<input type="text" name="tags" placeholder="Separate tags with a coma (,)" class="form-control"> 
+				</div>
+				
+				<div class="form-group">
+					<label for="">Post Status</label>
+					<select class="form-control" name="status">
+						<option value="draft">Draft</option>
+						<option value="published">Published</option>
+					</select>
+				</div>
+				
+				<div class="form-group">
+					<label for="">Post Image</label>
+					<input type="file" name="image" class="form-control"> 
+				</div>
+				
+				<div class="form-group">
+					<input type="submit" value="Publish Post" name="publish" class="btn btn-primary"> 
+				</div>
+			</form>
 		</div>
-		
-		<?php 
-			
-		?>
-		
 	</div>
-		<!-- /.row -->
-		
-		
-		
 </div>
-<!-- /.container-fluid -->
-</div>
-<!-- /#page-wrapper -->
-
-</div>
-<!-- /#wrapper -->
-
-<!-- jQuery -->
-<script src="bootstrap/js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
