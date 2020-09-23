03b3e61e77110176bafd8147b9ffe33cdbb8cd9b
marierabill <71690762+marierabill@users.noreply.github.com>
2020-09-23 11:07:03 +0300

    Update add_post.php

---
+++ admin/includes/add_post.php
@@ -0,0 +1,47 @@
+<div id="wrapper">
+
+	<!-- Navigation -->
+	<?php include 'includes/navigation.php'; ?>
+
+
+	<div id="page-wrapper">
+
+		<div class="container-fluid">
+
+			<!-- Page Heading -->
+			<div class="row">
+
+				
+				<h2>
+					Add Posts
+				</h2>
+				
+			</div>
+		</div>
+		
+		<?php 
+			
+		?>
+		
+	</div>
+		<!-- /.row -->
+		
+		
+		
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="bootstrap/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
