25455bdfff35172f21267610aa22f5488e2c3b10
marierabill <71690762+marierabill@users.noreply.github.com>
2020-10-23 12:32:55 +0300

    Update category.php

--- includes/category.php
+++ includes/category.php
@@ -1,12 +1,12 @@
 <?php
 	$query = "SELECT * FROM categories ORDER BY cat_id LIMIT 6";
 	$result = mysqli_query($connection, $query);
-	$span = mysqli_num_rows($result);
+	$cat_total_span = mysqli_num_rows($result);
 	
 ?>
 
 <div class="sidebar-box">
-	<h3 class="heading">Categories <?php echo "<span>[".$span."]</span>"; ?></h3>
+	<h3 class="heading">Categories <?php echo "<span>[".$cat_total_span."]</span>"; ?></h3>
 	<ul class="categories">
 <?php
 	while($row = mysqli_fetch_array($result))
@@ -14,10 +14,11 @@
 		$cat_id = $row['cat_id'];
 		$cat_title = $row['cat_title'];
 		
-		//echo "<li><a href='#'>$cat_title <span>($span)</span></a></li>";
+		$sql = mysqli_query($connection, "SELECT * FROM posts WHERE post_category_id = $cat_id");
+		$cat_span = mysqli_num_rows($sql);
 ?>
 	
-		<li><a href="#"><?php echo $cat_title ?> <span>(<?php echo $span ?>)</span></a></li>
+		<li><a href="category.php?cat_id=$cat_id"><?php echo $cat_title ?> <span>(<?php echo $cat_span ?>)</span></a></li>
 <?php	
 	}
 ?>
