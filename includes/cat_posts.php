f155db38f55c7dc8bdd5c292d6f0af293cf4d994
marierabill <71690762+marierabill@users.noreply.github.com>
2020-10-23 12:32:58 +0300

    Update cat_posts.php

--- includes/cat_posts.php
+++ includes/cat_posts.php
@@ -1,22 +1,30 @@
 <?php
 	(isset($_GET['cat_id'])) ? $cat_id = $_GET['cat_id'] : header("Location: index.php");
-	$query = "SELECT * FROM posts";
+	$query = "SELECT * FROM posts WHERE post_category_id = $cat_id ORDER BY post_id DESC";
 	$result = mysqli_query($connection, $query);
 	
-	while($row = mysqli_fetch_assoc($result))
+	if(mysqli_num_rows($result) === 0)
 	{
-		$post_id = $row['post_id'];
-		$post_title = $row['post_title'];
-		$post_author = $row['post_author'];
-		$post_category = $row['post_category'];
-		$post_category_id = $row['post_category_id'];
-		$post_content = $row['post_content'];
-		$post_tags = $row['post_tags'];
-		$post_status = $row['post_status'];
-		$post_image = $row['post_image'];		
-		$date = $row['post_date'];
-		$post_views = $row['post_views'];
-		$post_comment_count = $row['post_comment_count'];
+		echo "<h2>No posts available!</h2>";
+	}else
+	{
+		
+	
+	
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$post_id = $row['post_id'];
+			$post_title = $row['post_title'];
+			$post_author = $row['post_author'];
+			$post_category = $row['post_category'];
+			$post_category_id = $row['post_category_id'];
+			$post_content = $row['post_content'];
+			$post_tags = $row['post_tags'];
+			$post_status = $row['post_status'];
+			$post_image = $row['post_image'];		
+			$date = $row['post_date'];
+			$post_views = $row['post_views'];
+			$post_comment_count = $row['post_comment_count'];
 ?>
 <div class="col-md-6">
   <a href="single.php?post=<?php echo $post_id; ?>" class="blog-entry element-animate" data-animate-effect="fadeIn">
@@ -33,7 +41,7 @@
 </div>
 <?php
 	
+		}
 	}
-
 ?>
 
