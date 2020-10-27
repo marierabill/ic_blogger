e52f4a6849f2844a66e50fb7cd4e9bf8bf1dbfae
marierabill <71690762+marierabill@users.noreply.github.com>
2020-10-27 15:12:04 +0300

    Create create_admin.php

---
+++ create_admin.php
@@ -0,0 +1,39 @@
+<style>
+	.form
+	{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;		
+	}
+	.form input
+	{
+		width: 400px;
+	}
+</style>
+
+
+<form class="form" action="includes/form_handler/create.php" method="post" role="form">
+<h3>Create a New Admin</h3>
+	<div class="form-group">
+		<label>Username</label>
+		<input class="form-control" type="text" name="username" placeholder="Username">
+	</div>
+	<div class="form-group">
+		<label>Email Address</label>
+		<input class="form-control" type="email" name="email" placeholder="Email">
+	</div>
+	<div class="form-group">
+		<label>Password</label>
+		<input class="form-control" type="password" name="pwd" placeholder="Password" >
+		Suggested Passwords:
+		<ul>
+			<li><i><?php echo uniqid(); ?></i></li>
+			<li><i><?php echo uniqid(); ?></i></li>
+			<li><i><?php echo uniqid(); ?></i></li>
+		</ul>
+	</div>
+	<div class="form-group">
+		<input class="btn btn-primary" type="submit" name="create_submit" placeholder="Continue">
+	</div>
+</form>
