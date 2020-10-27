85b6a57bba2380f222b84aef896a22840f2073be
marierabill <71690762+marierabill@users.noreply.github.com>
2020-10-27 14:19:29 +0300

    Update comment.php

--- classes/comment.php
+++ classes/comment.php
@@ -44,6 +44,57 @@ class Comment
 		}
 		
 	}
+	
+	public function getComments
+	{
+		$query = mysqli_query($this->con, "SELECT * FROM comments ORDER BY id DESC");
+		
+		$str = "";
+		
+		while($row = mysqli_fetch_array($query))
+		{
+			$id = $row['id'];
+			$post_id = $row['post_id'];
+			$name = $row['comment_name'];
+			$body = $row['body'];
+			$email = $row['comment_email'];
+			$status = $row['status'];
+			
+			
+			$str .= "
+					<tr>
+						<td>$id</td>
+						<td>$name</td>
+						<td>$email</td>
+						<td>$body</td>
+						<td>$status</td>
+						<td>$post_id</td>
+					</tr>
+			
+			";
+		}
+		echo $str;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 ?>
