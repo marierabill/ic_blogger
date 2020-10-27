3dea732e334a7078457b0fd674f1512565691a92
marierabill <71690762+marierabill@users.noreply.github.com>
2020-10-27 15:12:08 +0300

    Update cms_admin.php

--- cms_admin.php
+++ cms_admin.php
@@ -1,3 +1,11 @@
 <?php
 	include "includes/header.php";
+	$query = mysqli_query($connection, "SELECT * FROM users");
+	if(mysqli_num_rows($query) === 0)
+	{
+		include "create_admin.php";
+	}else
+	{
+		include "login.php";
+	}
 ?>
